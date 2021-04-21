1a70188ead1ef2a40e84cb89fa54dcd1b82f3b5d
Javion <1617355924@qq.com>
2021-04-21 16:00:27 +0800

    完善Poller

--- net/Poller.cpp
+++ net/Poller.cpp
@@ -1,4 +1,6 @@
 #include "Poller.h"
+#include <cstdlib>
+#include <string>
 #include "EpollPoller.h"
 #include "PollPoller.h"
 #include "SelectPoller.h"
@@ -22,5 +24,13 @@ EventLoop * Poller::eventLoop() const {
 }
 
 Poller::PollerPtr Poller::createDefaultPoller(EventLoop * loop) {
+    std::string poller(getenv("TINY_SERVER_POLLER"));
+    if(poller == "EPOLL") {
+        return PollerPtr(new EpollPoller(loop));
+    } else if(poller == "POLL") {
+        return PollerPtr(new PollPoller(loop));
+    } else if(poller == "SELECT") {
+        return PollerPtr(new SelectPoller(loop));
+    }
     return PollerPtr(new EpollPoller(loop));
 }
