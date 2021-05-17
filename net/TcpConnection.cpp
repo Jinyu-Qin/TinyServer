087587b5f38fed1b01ef969bc6c4673470e1522b
Javion <1617355924@qq.com>
2021-05-17 23:48:55 +0800

    添加TcpConnection实现，封装了TCP连接的发送、读取、关闭等过程

---
+++ net/TcpConnection.cpp
@@ -0,0 +1,314 @@
+#include "TcpConnection.h"
+#include "EventLoop.h"
+#include "TimeStamp.h"
+#include "Channel.h"
+#include "Socket.h"
+#include <glog/logging.h>
+#include <cassert>
+
+TcpConnection::TcpConnection(EventLoop * loop, const std::string & name, int sockfd, const InetAddress & localAddr, const InetAddress & peerAddr)
+    : loop_(loop)
+    , name_(name)
+    , localAddr_(localAddr)
+    , peerAddr_(peerAddr)
+    , state_(kConnecting)
+    , reading_(false)
+    , socket_(new Socket(sockfd))
+    , channel_(new Channel(loop_, sockfd)) {
+    
+    // 只有在将this暴露给外部对象的时候，才可以使用shared_from_this()，否则，对象将永生不灭
+    channel_->setReadCallback(std::bind(&TcpConnection::handleRead, this, std::placeholders::_1));
+    channel_->setWriteCallback(std::bind(&TcpConnection::handleWrite, this));
+    channel_->setCloseCallback(std::bind(&TcpConnection::handleClose, this));
+    channel_->setErrorCallback(std::bind(&TcpConnection::handleError, this));
+
+    DLOG(INFO) << "TcpConnection::TcpConnection() called, name = " << name_
+               << ", sockfd = " << socket_->fd()
+               << ", localaddr = " << localAddr_
+               << ", peeraddr = " << peerAddr_;
+}
+
+TcpConnection::~TcpConnection() {
+    DLOG(INFO) << "TcpConnection::~TcpConnection() called, name = " << name_
+               << ", sockfd = " << socket_->fd()
+               << ", localaddr = " << localAddr_
+               << ", peeraddr = " << peerAddr_;
+    assert(state_ == kDisconnected);
+}
+
+EventLoop * TcpConnection::getLoop() const {
+    return loop_;
+}
+
+const std::string & TcpConnection::name() const {
+    return name_;
+}
+
+const InetAddress & TcpConnection::localAddress() const {
+    return localAddr_;
+}
+
+const InetAddress & TcpConnection::peerAddress() const {
+    return peerAddr_;
+}
+
+bool TcpConnection::connected() const {
+    return state_ == kConnected;
+}
+
+bool TcpConnection::disconnected() const {
+    return state_ == kDisconnected;
+}
+
+void TcpConnection::send(const void * message, size_t size) {
+    if(state_ != kConnected) {
+        LOG(WARNING) << "Ignore TcpConnection::send(), state = " << stateString(state_);
+        return ;
+    }
+
+    if(loop_->isInLoopThread()) {
+        sendInLoop(message, size);
+    } else {
+        std::shared_ptr<Buffer> buf = std::make_shared<Buffer>(size);
+        buf->write(static_cast<const char *>(message), size);
+        void (TcpConnection::*fp)(std::shared_ptr<Buffer>) = &TcpConnection::sendInLoop;
+        // FIXME this or shared_from_this()?
+        loop_->queueInLoop(std::bind(fp, shared_from_this(), buf));
+    }
+}
+
+void TcpConnection::send(BufferPtr message) {
+    Buffer::size_type readableSize = message->readableSize();
+    send(message->readBegin(), readableSize);
+    message->hasRead(readableSize);
+}
+
+void TcpConnection::shutdown() {
+    if(state_ != kConnected) {
+        LOG(WARNING) << "Ignore TcpConnection::shutdown(), state = " << stateString(state_);
+        return ;
+    }
+    state_ = kDisconnecting;
+    // FIXME this or shared_from_this()?
+    loop_->runInLoop(std::bind(&TcpConnection::shutdownInLoop, shared_from_this()));
+}
+
+void TcpConnection::forceClose() {
+    if(state_ != kConnected) {
+        LOG(WARNING) << "Ignore TcpConnection::forceClose(), state = " << stateString(state_);
+        return ;
+    }
+    state_ = kDisconnecting;
+    // FIXME this or shared_from_this()?
+    loop_->runInLoop(std::bind(&TcpConnection::forceCloseInLoop, shared_from_this()));
+}
+
+void TcpConnection::startRead() {
+    // FIXME check state?
+    // FIXME this or shared_from_this()?
+    loop_->runInLoop(std::bind(&TcpConnection::startReadInLoop, shared_from_this()));
+}
+
+void TcpConnection::stopRead() {
+    // FIXME check state?
+    // FIXME this or shared_from_this()?
+    loop_->runInLoop(std::bind(&TcpConnection::stopReadInLoop, shared_from_this()));
+}
+
+bool TcpConnection::isReading() const {
+    return reading_;
+}
+
+void TcpConnection::setConnectionCallback(ConnectionCallback callback) {
+    connectionCallback_ = callback;
+}
+
+void TcpConnection::setMessageCallback(MessageCallback callback) {
+    messageCallback_ = callback;
+}
+
+void TcpConnection::setWriteCompleteCallback(WriteCompleteCallback callback) {
+    writeCompleteCallback_ = callback;
+}
+
+void TcpConnection::setCloseCallback(CloseCallback callback) {
+    closeCallback_ = callback;
+}
+
+void TcpConnection::connectEstablished() {
+    loop_->assertInLoopThread();
+    assert(state_ == kConnecting);
+    // FIXME 这里脱离了reading_的掌控
+    channel_->enableReading();
+    state_ = kConnected;
+
+    connectionCallback_(shared_from_this());
+}
+
+void TcpConnection::connectDestroyed() {
+    loop_->assertInLoopThread();
+    if(state_ == kConnected) {
+        state_ = kDisconnected;
+        channel_->disableAll();
+        
+        if(connectionCallback_) {
+            connectionCallback_(shared_from_this());
+        }
+    }
+    channel_->remove();
+}
+
+void TcpConnection::handleRead(TimeStamp receiveTime) {
+    loop_->assertInLoopThread();
+
+    Buffer::size_type nBytes = inputBuffer_.writeFromFd(socket_->fd());
+    if(nBytes > 0) {
+        // 读到nBytes字节数据
+        if(messageCallback_) {
+            messageCallback_(shared_from_this(), &inputBuffer_, receiveTime);
+        }
+    } else if(nBytes == 0) {
+        // 对端关闭连接
+        DLOG(INFO) << "The peer (" << peerAddr_ << ") closed the tcp connection";
+        handleClose();
+    } else {
+        // 出错
+        handleError();
+    }
+}
+
+void TcpConnection::handleWrite() {
+    loop_->assertInLoopThread();
+    assert(channel_->isWriting());
+
+    Buffer::size_type nBytes = outputBuffer_.readIntoFd(socket_->fd());
+    if(nBytes >= 0) {
+        // 写入nBytes字节
+        if(outputBuffer_.readableSize() == 0) {
+            // 缓冲区全部输出
+            channel_->disableWriting();
+            if(writeCompleteCallback_) {
+                writeCompleteCallback_(shared_from_this());
+            }
+
+            // 缓冲区数据全部输出，可以关闭连接了
+            if(state_ == kDisconnecting) {
+                shutdownInLoop();
+            }
+        }
+    } else {
+        // 出错
+        handleClose();
+    }
+}
+
+void TcpConnection::handleClose() {
+    loop_->assertInLoopThread();
+    // kDisconnecting状态表示主动关闭，kConnected状态表示被动关闭
+    assert(state_ == kDisconnecting || state_ == kConnected);
+
+    state_ = kDisconnected;
+    channel_->disableAll();
+    if(connectionCallback_) {
+        connectionCallback_(shared_from_this());
+    }
+    if(closeCallback_) {
+        closeCallback_(shared_from_this());
+    }
+}
+
+void TcpConnection::handleError() {
+    loop_->assertInLoopThread();
+
+    LOG(FATAL) << "An Error happened in TcpConnection [name = " << name_
+               << ", sockfd = " << socket_->fd()
+               << ", localaddr = " << localAddr_
+               << ", peeraddr = " << peerAddr_ << "]";
+}
+
+void TcpConnection::sendInLoop(const void * message, size_t size) {
+    loop_->assertInLoopThread();
+    assert(state_ == kConnected);
+
+    ssize_t remaining = size;
+    ssize_t nBytes = 0;
+    bool error = false;
+    // 首先尝试直接发送
+    if(!channel_->isWriting() && outputBuffer_.readableSize() == 0) {
+        nBytes = ::send(socket_->fd(), message, size, 0);
+        if(nBytes == remaining) {
+            // 全部直接发送完成
+            remaining -= nBytes;
+            if(writeCompleteCallback_) {
+                writeCompleteCallback_(shared_from_this());
+            }
+        } else if(nBytes >= 0) {
+            // 部分直接发送完成
+            remaining -= nBytes;
+        } else {
+            error = true;
+            LOG(ERROR) << "Something wrong when call bind() in TcpConnection::sendInLoop(const void * message, size_t size), the errno is " << errno << "(" << strerror(errno) << ")";
+            handleError();
+        }
+    }
+
+    // 然后尝试缓冲发送
+    if(!error && remaining > 0) {
+        outputBuffer_.write(static_cast<const char *>(message) + nBytes, remaining);
+        if(!channel_->isWriting()) {
+            channel_->enableWriting();
+        }
+    }
+}
+
+
+void TcpConnection::sendInLoop(std::shared_ptr<Buffer> message) {
+    loop_->assertInLoopThread();
+    sendInLoop(message->readBegin(), message->readableSize());
+}
+
+void TcpConnection::shutdownInLoop() {
+    loop_->assertInLoopThread();
+    assert(state_ == kDisconnecting);
+    
+    // 没数据需要发送了才会执行
+    if(!channel_->isWriting()) {
+        socket_->shutdownWrite();
+    }
+    // 若还有数据需要发送，则待数据发送完毕后，会自动再次调用shutdownInLoop()
+}
+
+void TcpConnection::forceCloseInLoop() {
+    loop_->assertInLoopThread();
+    assert(state_ == kDisconnecting || state_ == kConnected);
+
+    handleClose();
+}
+
+void TcpConnection::startReadInLoop() {
+    loop_->assertInLoopThread();
+    if(!reading_ || !channel_->isReading()) {
+        channel_->enableReading();
+        reading_ = true;
+    }
+}
+
+void TcpConnection::stopReadInLoop() {
+    loop_->assertInLoopThread();
+
+    if(reading_ || channel_->isReading()) {
+        reading_ = false;
+        channel_->disableReading();
+    }
+}
+
+const std::string & TcpConnection::stateString(TcpConnectionState state) {
+    return stateStr[state];
+}
+
+const std::string TcpConnection::stateStr[] = {
+    "kConnecting",
+    "kConnected",
+    "kDisconnecting",
+    "kDisconnected"
+};
---
+++ net/inc/TcpConnection.h
@@ -0,0 +1,117 @@
+#ifndef __TCPCONNECTION_H__
+#define __TCPCONNECTION_H__
+
+#include <boost/utility.hpp>
+#include <memory>
+#include <string>
+#include <functional>
+#include "InetAddress.h"
+#include "Buffer.h"
+
+class EventLoop;
+class Channel;
+class Socket;
+class TimeStamp;
+
+class TcpConnection: public boost::noncopyable, public std::enable_shared_from_this<TcpConnection> {
+public:
+    using BufferPtr             = Buffer *;
+    using TcpConnectionPtr      = std::shared_ptr<TcpConnection>;
+    
+    using ConnectionCallback    = std::function<void(TcpConnectionPtr)>;
+    using CloseCallback         = std::function<void(TcpConnectionPtr)>;
+    using WriteCompleteCallback = std::function<void(TcpConnectionPtr)>;
+    using MessageCallback       = std::function<void(TcpConnectionPtr, BufferPtr, TimeStamp)>;
+
+    TcpConnection(EventLoop * loop, const std::string & name, int sockfd, const InetAddress & localAddr, const InetAddress & peerAddr);
+    ~TcpConnection();
+
+    // 获取TcpConnection所绑定的EventLoop
+    EventLoop * getLoop() const;
+    // 获取TcpConnection的名称
+    const std::string & name() const;
+    // 获取本地地址
+    const InetAddress & localAddress() const;
+    // 获取对端地址
+    const InetAddress & peerAddress() const;
+    // 是否已建立连接
+    bool connected() const;
+    // 是否已断开连接
+    bool disconnected() const;
+
+    // 发送数据
+    void send(const void * message, size_t size);
+    // 发送数据
+    void send(BufferPtr message);
+
+    // 半关闭
+    void shutdown();
+    // 全关闭
+    void forceClose();
+
+    // 开始读
+    void startRead();
+    // 停止读
+    void stopRead();
+    // 是否正在读
+    bool isReading() const;
+
+    // 设置连接回调函数（建立连接和断开连接时都会调用）
+    void setConnectionCallback(ConnectionCallback callback);
+    // 设置接收完消息回调函数
+    void setMessageCallback(MessageCallback callback);
+    // 设置发送完消息回调函数
+    void setWriteCompleteCallback(WriteCompleteCallback callback);
+    // 设置连接关闭回调函数（仅供TcpServer调用）
+    void setCloseCallback(CloseCallback callback);
+
+    // 连接建立时调用（仅供TcpServer调用一次）
+    void connectEstablished();
+    // 连接断开时调用（仅供TcpServer调用一次）
+    void connectDestroyed();
+
+private:
+    enum TcpConnectionState {
+        kConnecting,
+        kConnected,
+        kDisconnecting,
+        kDisconnected
+    };
+
+    void handleRead(TimeStamp receiveTime);
+    void handleWrite();
+    void handleClose();
+    void handleError();
+
+    void sendInLoop(const void * message, size_t size);
+    void sendInLoop(std::shared_ptr<Buffer> message);
+    void shutdownInLoop();
+    void forceCloseInLoop();
+    void startReadInLoop();
+    void stopReadInLoop();
+
+    static const std::string & stateString(TcpConnectionState state);
+
+    EventLoop * loop_;                  // TcpConnection所绑定的EventLoop对象
+    const std::string name_;            // TcpConnection名称
+    const InetAddress localAddr_;       // 本地地址
+    const InetAddress peerAddr_;        // 对端地址
+
+    TcpConnectionState state_;          // TcpConnection所处状态
+    bool reading_;                      // 标识是否正在读
+    std::unique_ptr<Socket> socket_;    // TcpConnection持有的Socket对象
+    std::unique_ptr<Channel> channel_;  // TcpConnectione持有的Channel对象
+
+    ConnectionCallback connectionCallback_;         // 连接回调函数
+    MessageCallback messageCallback_;               // 读完成回调函数
+    WriteCompleteCallback writeCompleteCallback_;   // 写完成回调函数
+    CloseCallback closeCallback_;                   // 连接关闭回调函数
+
+    Buffer inputBuffer_;                // 接收缓冲
+    Buffer outputBuffer_;               // 发送缓冲
+
+    static const std::string stateStr[];
+};
+
+
+#endif //__TCPCONNECTION_H__
